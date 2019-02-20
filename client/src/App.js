4c8f9e2e35861aa5bf21f8c4629965dfbe9963af
Lucas Fredin <ii.fredin@gmail.com>
2019-02-20 21:22:58 +0100

    Small changes, and added some new files and folders to be used

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-     
+        <h1>placeholder title</h1>
       </div>
     );
   }
---
+++ client/src/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const GET_POSTS = 'GET_POSTS';
+export const GET_POST = 'GET_POST';
+export const LOADING_POSTS = 'LOADING_POSTS';
+
+export const GET_IMAGES = 'GET_IMAGES';
+export const GET_IMAGE = 'GET_IMAGE';
+export const LOADING_IMAGES = 'LOADING_IMAGES';
