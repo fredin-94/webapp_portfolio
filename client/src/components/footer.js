2f693e59163cd3814ac3dcf8c8034ad732881916
Lucas Fredin <ii.fredin@gmail.com>
2019-04-16 19:46:18 +0200

    testing a few different styles on the home page

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,13 +3,13 @@ import React, {Component} from 'react';
 class footer extends Component{
     render(){
         return(
-            <div>
+            <div>  
                 <div className="footerMain">
                     <div className="container">
                         <span className="text-muted">try from the internet.</span>
                         insert links here to github and all those places as icons or something
                     </div>
-                </div>
+                </div>       
             </div>
         );
     }
--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -5,9 +5,16 @@ class Homepage extends Component{
     render(){
         return(
             <div>
-                <h1 className="headText backText">Lucas</h1>
-                <h1 className="headText frontText">Fredin</h1>
-                <h2>Insert some text here about me or what i can do or about this website or quote or something, ya know, like adults do!</h2>
+                <div class="innerContainer">
+                    <h1 className="headText backText">Lucas</h1>
+                    <h1 className="headText frontText">Fredin</h1>
+                    <h3 className="contentText">Insert some text here about me or what i can do or about this website or quote or something, ya know, like adults do!</h3>
+                </div>
+                <div>
+                    <a href="/images">My art</a> 
+                    <a href="/posts">My blog</a> 
+                    <a href="/projects">My dev projects</a> 
+                </div>
                 
                 <Button color="primary">primary</Button>{' '}
             </div>
