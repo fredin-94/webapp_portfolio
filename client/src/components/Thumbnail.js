4e17c8dc115790c70856c75aad0c561c8451643a
Lucas Fredin <ii.fredin@gmail.com>
2019-07-11 20:06:40 +0200

    making the thumbnails better

--- client/src/components/Thumbnail.js
+++ client/src/components/Thumbnail.js
@@ -2,21 +2,37 @@ import React from 'react';
 
 import '../styles/ImgGrid.css';
 import '../styles/Thumbnail.css';
+import '../styles/linkStyle.css';
 
 const Thumbnail = (props)=>{
     
     //should add LINK to the project too
-    const images = props.images.map(({imgurl, title, year, _id, tags})=>{ //this is kinda like a forloop for all images we got, then we save the results in a new array called images
+    const images = props.images.map(({imgurl, title, year, _id, tags, projecturl})=>{ //this is kinda like a forloop for all images we got, then we save the results in a new array called images
         //use destructuring above to get the properties of the "image"
         if(imgurl === "" || imgurl===null){
             imgurl = "https://cdn11.bigcommerce.com/s-gho61/stencil/31cc7cb0-5035-0136-2287-0242ac11001b/e/3dad8ea0-5035-0136-cda0-0242ac110004/images/no-image.svg";
         }
 
+
+        //let pTags = tags.join(" ");
+
+        function getText(tag){ //trying to get tags to be separated
+            let text = "";
+
+            text = text + tag + " ";
+
+            return text;
+        }
+
         return(
             <div className="small-img" key={_id}> 
-                <img src={imgurl} alt={title}/>
-                <p className="project-tags">{tags}</p>
-                <h4 className="img-title">{title}</h4>
+                <li>
+                    <a rel="noopener noreferrer" target="_blank" href={projecturl}>
+                        <img src={imgurl} alt={title}/>
+                        <p className="project-tags">{tags}</p>
+                        <h4 className="img-title">{title}</h4>
+                    </a>
+                </li>
                 <p>{year}</p>
             </div>   
         );
