9c8c8e93df0127998a041417aeaed30345232bc9
Lucas Fredin <ii.fredin@gmail.com>
2019-07-27 17:44:47 +0200

    every other picture

--- client/src/components/ListProjects.js
+++ client/src/components/ListProjects.js
@@ -3,11 +3,13 @@ import {Row, Col} from 'reactstrap';
 
 import '../styles/ProjectListStyle.css';
 
+var counter = 1;
+
 const ListProjects = (props)=>{
     const projects = props.projects.map(({imgurl, title, year, _id, tags, projecturl})=>{ //this is kinda like a forloop for all images we got, then we save the results in a new array called images
         
         if(imgurl === "" || imgurl===null){
-            imgurl = "https://cdn11.bigcommerce.com/s-gho61/stencil/31cc7cb0-5035-0136-2287-0242ac11001b/e/3dad8ea0-5035-0136-cda0-0242ac110004/images/no-image.svg";
+            imgurl = "https://www.tibco.com/blog/wp-content/uploads/2016/12/rsz_bigstock-programming-code-abstract-tech-131906921.jpg";
         }
 
         console.log("-------------->LIST PROJECTS:" + title);
@@ -16,22 +18,44 @@ const ListProjects = (props)=>{
             backgroundImage: 'url(' + imgurl + ')'
         }
 
-        return(
-            //<li className="d-flex justify-content-between align-items-center" key={_id}> 
-                <Row key={_id} className="space">
-                    <Col xs="4" className="noSidePad img-area">
-                        <div className="listSidePicture" style={bgStyle}></div>
-                    </Col>
-                    <Col xs="8" className="text-area">
-                        <a rel="noopener noreferrer" target="_blank" href={projecturl}><h4>{title}</h4></a>
-                        <h6>{year}</h6>
-                        <p>Tags: {tags}</p>
-                        <p>Description: (make description in mongolab and model on server and put here)</p>
-                    </Col>
-                </Row>
-                
-            //</li>   
-        );
+
+        if(counter % 2 === 0){
+            counter++;
+            return(
+                //<li className="d-flex justify-content-between align-items-center" key={_id}> 
+                    <Row key={_id} className="space">
+                        <Col xs="8" className="text-area">
+                            <a rel="noopener noreferrer" target="_blank" href={projecturl}><h4>{title}</h4></a>
+                            <h6>{year}</h6>
+                            <p>Tags: {tags}</p>
+                            <p>Description: (make description in mongolab and model on server and put here)</p>
+                        </Col>
+                        <Col xs="4" className="noSidePad img-area">
+                            <div className="listSidePicture" style={bgStyle}></div>
+                        </Col>
+                    </Row>
+                    
+                //</li>   
+            );
+        }else{
+            counter++;
+            return(
+                //<li className="d-flex justify-content-between align-items-center" key={_id}> 
+                    <Row key={_id} className="space">
+                        <Col xs="4" className="noSidePad img-area">
+                            <div className="listSidePicture" style={bgStyle}></div>
+                        </Col>
+                        <Col xs="8" className="text-area">
+                            <a rel="noopener noreferrer" target="_blank" href={projecturl}><h4>{title}</h4></a>
+                            <h6>{year}</h6>
+                            <p>Tags: {tags}</p>
+                            <p>Description: (make description in mongolab and model on server and put here)</p>
+                        </Col>
+                    </Row>
+                    
+                //</li>   
+            );
+        }
     });
 
     return(
